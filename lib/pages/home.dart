f3842405c1ad607394fbc2e0e959c6d5fa662a38
Alyshow <alyson.corinthians@gmail.com>
2021-01-09 14:02:18 -0300

    commit final 3

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import './../colors/colors.dart';
 import '../token_service.dart';
 import 'dart:convert';
+import 'Employee.dart';
+import 'Services.dart';
+
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
 
@@ -30,7 +33,7 @@ class _HomePageState extends State<HomePage> {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget> [
           Center(
-            child: Text('WELCOME BACK, STUDENT',
+            child: Text('WELCOME BACK, TEACHER',
               style: TextStyle (
                 fontWeight: FontWeight.bold,
                 fontFamily: 'IndieFlower',
@@ -64,7 +67,7 @@ class _HomePageState extends State<HomePage> {
               )
           ),
           RaisedButton(
-              onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context) => Editar()));},
+              onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context) => DataTableDemo()));},
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(80.0)
               ),
@@ -714,5 +717,294 @@ class Editar extends StatelessWidget{
       backgroundColor: Colors.blue[300],
     );
 
+  }
+}
+
+class DataTableDemo extends StatefulWidget {
+  //
+  DataTableDemo() : super();
+
+
+  final String title = 'Editar aulas';
+
+  @override
+  DataTableDemoState createState() => DataTableDemoState();
+}
+
+class DataTableDemoState extends State<DataTableDemo> {
+  List<Employee> _employees;
+  GlobalKey<ScaffoldState> _scaffoldKey;
+  // controller for the First Name TextField we are going to create.
+  TextEditingController _firstNameController;
+  // controller for the Last Name TextField we are going to create.
+  TextEditingController _lastNameController;
+  Employee _selectedEmployee;
+  bool _isUpdating;
+  String _titleProgress;
+
+  @override
+  void initState() {
+    super.initState();
+    _employees = [];
+    _isUpdating = false;
+    _titleProgress = widget.title;
+    _scaffoldKey = GlobalKey(); // key to get the context to show a SnackBar
+    _firstNameController = TextEditingController();
+    _lastNameController = TextEditingController();
+    _getEmployees();
+  }
+
+  // Method to update title in the AppBar Title
+  _showProgress(String message) {
+    setState(() {
+      _titleProgress = message;
+    });
+  }
+
+  _showSnackBar(context, message) {
+    _scaffoldKey.currentState.showSnackBar(
+      SnackBar(
+        content: Text(message),
+      ),
+    );
+  }
+
+  _createTable() {
+    _showProgress('Criando tabela...');
+    Services.createTable().then((result) {
+      if ('success' == result) {
+        // Table is created successfully.
+        _showSnackBar(context, result);
+        _showProgress(widget.title);
+      }
+    });
+  }
+
+  // Now lets add an Employee
+  _addEmployee() {
+    if (_firstNameController.text.isEmpty || _lastNameController.text.isEmpty) {
+      print('Campos vazios');
+      return;
+    }
+    _showProgress('Adicionando aula...');
+    Services.addEmployee(_firstNameController.text, _lastNameController.text)
+        .then((result) {
+      if ('success' == result) {
+        _getEmployees(); // Refresh the List after adding each employee...
+        _clearValues();
+      }
+    });
+  }
+
+  _getEmployees() {
+    _showProgress('Carregando aulas...');
+    Services.getEmployees().then((employees) {
+      setState(() {
+        _employees = employees;
+      });
+      _showProgress(widget.title); // Reset the title...
+      print("Length ${employees.length}");
+    });
+  }
+
+  _updateEmployee(Employee employee) {
+    setState(() {
+      _isUpdating = true;
+    });
+    _showProgress('Atualizando aulas...');
+    Services.updateEmployee(
+        employee.id, _firstNameController.text, _lastNameController.text)
+        .then((result) {
+      if ('success' == result) {
+        _getEmployees(); // Refresh the list after update
+        setState(() {
+          _isUpdating = false;
+        });
+        _clearValues();
+      }
+    });
+  }
+
+  _deleteEmployee(Employee employee) {
+    _showProgress('Apagando aula...');
+    Services.deleteEmployee(employee.id).then((result) {
+      if ('success' == result) {
+        _getEmployees(); // Refresh after delete...
+      }
+    });
+  }
+
+  // Method to clear TextField values
+  _clearValues() {
+    _firstNameController.text = '';
+    _lastNameController.text = '';
+  }
+
+  _showValues(Employee employee) {
+    _firstNameController.text = employee.firstName;
+    _lastNameController.text = employee.lastName;
+  }
+
+  // Let's create a DataTable and show the employee list in it.
+  SingleChildScrollView _dataBody() {
+    // Both Vertical and Horozontal Scrollview for the DataTable to
+    // scroll both Vertical and Horizontal...
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: DataTable(
+          columns: [
+            DataColumn(
+              label: Text('ID'),
+            ),
+            DataColumn(
+              label: Text('ASSUNTO'),
+            ),
+            DataColumn(
+              label: Text('MATÉRIA'),
+            ),
+            // Lets add one more column to show a delete button
+            DataColumn(
+              label: Text('DELETAR'),
+            )
+          ],
+          rows: _employees
+              .map(
+                (employee) => DataRow(cells: [
+              DataCell(
+                Text(employee.id),
+                // Add tap in the row and populate the
+                // textfields with the corresponding values to update
+                onTap: () {
+                  _showValues(employee);
+                  // Set the Selected employee to Update
+                  _selectedEmployee = employee;
+                  setState(() {
+                    _isUpdating = true;
+                  });
+                },
+              ),
+              DataCell(
+                Text(
+                  employee.firstName.toUpperCase(),
+                ),
+                onTap: () {
+                  _showValues(employee);
+                  // Set the Selected employee to Update
+                  _selectedEmployee = employee;
+                  // Set flag updating to true to indicate in Update Mode
+                  setState(() {
+                    _isUpdating = true;
+                  });
+                },
+              ),
+              DataCell(
+                Text(
+                  employee.lastName.toUpperCase(),
+                ),
+                onTap: () {
+                  _showValues(employee);
+                  // Set the Selected employee to Update
+                  _selectedEmployee = employee;
+                  setState(() {
+                    _isUpdating = true;
+                  });
+                },
+              ),
+              DataCell(IconButton(
+                icon: Icon(Icons.delete),
+                onPressed: () {
+                  _deleteEmployee(employee);
+                },
+              ))
+            ]),
+          )
+              .toList(),
+        ),
+      ),
+    );
+  }
+
+  // UI
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        title: Text(_titleProgress), // we show the progress in the title...
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              _createTable();
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: () {
+              _getEmployees();
+            },
+          )
+        ],
+      ),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(20.0),
+              child: TextField(
+                controller: _firstNameController,
+                decoration: InputDecoration.collapsed(
+                  hintText: 'Assunto',
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(20.0),
+              child: TextField(
+                controller: _lastNameController,
+                decoration: InputDecoration.collapsed(
+                  hintText: 'Matéria',
+                ),
+              ),
+            ),
+            // Add an update button and a Cancel Button
+            // show these buttons only when updating an employee
+            _isUpdating
+                ? Row(
+              children: <Widget>[
+                OutlineButton(
+                  child: Text('UPDATE'),
+                  onPressed: () {
+                    _updateEmployee(_selectedEmployee);
+                  },
+                ),
+                OutlineButton(
+                  child: Text('CANCEL'),
+                  onPressed: () {
+                    setState(() {
+                      _isUpdating = false;
+                    });
+                    _clearValues();
+                  },
+                ),
+              ],
+            )
+                : Container(),
+            Expanded(
+              child: _dataBody(),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _addEmployee();
+        },
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
